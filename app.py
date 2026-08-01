10a18d897f4f70716e80f825649c753f9f56e278
ueslyfduarte <ueslyfduarte@gmail.com>
2026-08-01 00:18:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+# Garante que o diretório do app está no path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
 import pandas as pd
 import numpy as np
